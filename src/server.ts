63b61972d20d50dbf83028acbb8b99f1e3332f61
Dev-dfm <d.floresmedina@gmail.com>
2021-05-10 17:44:07 +0200

    ➕ Add questions for password

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,15 @@
-import fs from 'fs';
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-console.log(fs.readFileSync('./README.md', 'utf-8'));
+readline.question(`What password? `, (password: string) => {
+  console.log(`The password is ${password}!`);
+  readline.question(
+    `What should the password be used for? `,
+    (usage: string) => {
+      console.log(`Password usage: ${usage}!`);
+      readline.close();
+    }
+  );
+});
