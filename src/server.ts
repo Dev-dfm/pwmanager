7fd9a5e0e1f76960ce7a5f7d7e0a90bda1bfbba1
Dev-dfm <d.floresmedina@gmail.com>
2021-05-11 14:12:30 +0200

    delete code

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,1 @@
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
 
-readline.question(`What password? `, (password: string) => {
-  console.log(`The password is ${password}!`);
-  readline.question(
-    `What should the password be used for? `,
-    (usage: string) => {
-      console.log(`Password usage: ${usage}!`);
-      readline.close();
-    }
-  );
-});
