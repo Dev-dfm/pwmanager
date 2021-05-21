2873945506d0f8de10bdbda90fe5cf4adeff2451
Dev-dfm <d.floresmedina@gmail.com>
2021-05-21 17:28:04 +0200

    ✨ Fetch credentials from API

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,35 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Credential as CredentialType } from '../types';
 import styles from './App.module.css';
 import Credential from './components/Credential';
 import Hero from './components/Hero';
 
 function App(): JSX.Element {
-  const credentials: CredentialType[] = [
-    {
-      service: 'GitHub',
-      username: 'JuJu',
-      password: '123',
-    },
-    {
-      service: 'Google',
-      username: 'Roman',
-      password: '456',
-    },
-    {
-      service: 'Netflix',
-      username: 'Daniel',
-      password: '789',
-    },
-  ];
-  const crendentialElements = credentials.map((credential) => (
+  const [credentials, setCredentials] = useState<CredentialType[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/credentials')
+      .then((response) => response.json())
+      .then((credentials) => setCredentials(credentials));
+  }, []);
+  const credentialElements = credentials.map((credential) => (
     <Credential key={credential.service} credential={credential} />
   ));
 
   return (
     <div className={styles.App}>
-      <Hero title="Key Guardian" imgSrc="https://images.unsplash.com/photo-1584433144859-1fc3ab64a957?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1776&q=80" />
+      <Hero
+        title="Key Guardian"
+        imgSrc="https://images.unsplash.com/photo-1584433144859-1fc3ab64a957?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1776&q=80"
+      />
       <main>
-        <ul>{crendentialElements}</ul>
+        <ul>{credentialElements}</ul>
       </main>
     </div>
   );
